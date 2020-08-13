2188e79a23aa64dc4b0765c77df92da470dade9e
RuzaikMh <ruzaik42@gmail.com>
2020-08-13 17:56:19 +0530

    8/13

---
+++ app/src/main/java/com/example/childtracking/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.childtracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnLiveLocation(View view){
+        startActivity(new Intent(getApplicationContext(),LiveLocation.class));
+    }
+
+}
